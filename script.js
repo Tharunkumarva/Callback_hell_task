fcb10064b256212879c6f8e0b535458c1338fd7d
THARUN KUMAR V A <124011988+Tharunkumarva@users.noreply.github.com>
2023-10-04 21:14:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function countdownFrom(num, callback) {
-    if (num === 0) {
-        callback();
-    } else {
-        document.getElementById('countdown').innerText = num;
-        setTimeout(function () {
-            countdownFrom(num - 1, callback);
-        }, 1000);
-    }
+function countdownFrom(num) {
+    return new Promise(function (resolve, reject) {
+        function countdown() {
+            if (num === 0) {
+                resolve();
+            } else {
+                document.getElementById('countdown').innerText = num;
+                setTimeout(function () {
+                    num--;
+                    countdown();
+                }, 1000);
+            }
+        }
+        countdown();
+    });
 }
 
-countdownFrom(10, function () {
+function showMessage() {
     document.getElementById('countdown').innerText = 'Happy Independence Day';
-});
+    document.getElementById('message').innerText = 'Enjoy the celebration!';
+}
+
+countdownFrom(10)
+    .then(showMessage)
+    .catch(function (error) {
+        console.error(error);
+    });
